b105a8989feb7418e02724b5cff9c336c2f4dcc2
Ashley Preece <ashleyjph@gmail.com>
2025-01-16 10:37:58 +0000

    Prep secret key before deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2nwzk-hec=0bl%0!rshmi1@-7(hl1@oo2@qy!9%+tm*58_khpi'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ash5p-sixblokes-mk7j3nbe1mi.ws.codeinstitute-ide.net',
     '.herokuapp.com']
