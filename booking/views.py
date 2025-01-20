06799a0127705107a56f0e9240d52293a04f1326
Ashley Preece <ashleyjph@gmail.com>
2025-01-20 20:38:40 +0000

    Add and style allauth login

--- booking/views.py
+++ booking/views.py
@@ -7,3 +7,8 @@ from .forms import BookingForm
 # Create your views here.
 class MakeBooking(generic.TemplateView):
     template_name = "booking/booking.html"
+
+def booking_view(request):
+    context ={}
+    context['form']= BookingForm()
+    return render(request, "booking.html", context)
