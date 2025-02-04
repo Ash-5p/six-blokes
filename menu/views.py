4c9d830e60bf0106253b6f38b6945fd2e58065d1
Ash-5p <ashleyjph@gmail.com>
2025-02-04 21:12:28 +0000

    Add DTL tags for menu items

--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 
 # Create your views here.
+
+
 def menu(request):
+    
+    menu_starters = MenuItem.objects.filter(category="starter")
+    
     return render(
         request,
         "menu/menu.html",
         {
             "menu": "menu",
+            "menu_starters": menu_starters
         }
         )
