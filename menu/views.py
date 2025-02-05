36fcbf5fbd4fc4aeab85efa7f6d275aa8845be25
Ash-5p <ashleyjph@gmail.com>
2025-02-05 20:23:58 +0000

    Impliment CRUD functionality for menu

--- menu/views.py
+++ menu/views.py
@@ -8,12 +8,21 @@ from .models import MenuItem
 def menu(request):
     
     menu_starters = MenuItem.objects.filter(category="starter")
+    menu_burgers = MenuItem.objects.filter(category="burger")
+    menu_sides = MenuItem.objects.filter(category="side")
+    menu_desserts = MenuItem.objects.filter(category="dessert")
+    menu_drinks = MenuItem.objects.filter(category="drink")
+
+    context = {
+        "menu_starters": menu_starters,
+        "menu_burgers": menu_burgers,
+        "menu_sides": menu_sides,
+        "menu_desserts": menu_desserts,
+        "menu_drinks": menu_drinks
+    }
     
     return render(
         request,
         "menu/menu.html",
-        {
-            "menu": "menu",
-            "menu_starters": menu_starters
-        }
+        context,
         )
